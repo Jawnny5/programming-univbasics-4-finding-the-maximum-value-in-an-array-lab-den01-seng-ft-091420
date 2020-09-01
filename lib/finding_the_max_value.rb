157e444d9685fceca245a30972a12d1d926b338c
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-01 23:05:43 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,9 @@
 def find_max_value(array)
   count = 0
   while count < array.length do
-    if count == find_max_value([array])
+    if count == find_max_value[array)
       return count
+    end
+    count += 1
   end
-  count += 1
 end
