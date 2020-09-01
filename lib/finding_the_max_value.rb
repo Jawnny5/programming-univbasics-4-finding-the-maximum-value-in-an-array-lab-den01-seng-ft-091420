600451f2604b0d3751f3691483d8def15d2f7ebd
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-01 23:01:31 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,8 @@
 def find_max_value(array)
-  # Add your solution here
+  count = 0
+  while count < array.length do
+    if count == find_max_value(array)
+      return count
+  end
+  count += 1
 end
